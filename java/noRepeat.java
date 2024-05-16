117cdccd3f17788d61014d498f4b154e3f51e258
Mateo <mateosolino1@gmail.com>
2024-05-16 11:53:21 +0200

    Add noRepeat.java

---
+++ java/noRepeat.java
@@ -0,0 +1,43 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode newHead = head;
+        while (newHead != null && newHead.next != null) {
+            if (newHead.val == newHead.next.val) {
+                newHead.next = newHead.next.next;
+            } else {
+                newHead = newHead.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+        Solution solution = new Solution();
+
+        solution.deleteDuplicates(head);
+
+    }
+}
